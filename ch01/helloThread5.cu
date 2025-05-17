f8ee539b03ace45bdaa1b0634cbf9330419a2432
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2025-05-17 17:32:58 -0500

    added chapter 01 exercises

---
+++ ch01/helloThread5.cu
@@ -0,0 +1,20 @@
+// print only thread id == 5's output
+
+#include <stdio.h>
+
+__global__ void helloFromGPU(void) {
+    if (threadIdx.x == 5) {
+        printf("Hello World from GPU! thread %d!\n", threadIdx.x);
+    }
+}
+
+int main(void)
+{
+    // hello from cpu
+    printf("Hello World from CPU!\n");
+
+    helloFromGPU<<<1, 10>>>();
+    cudaDeviceSynchronize();
+
+    return 0;
+}
